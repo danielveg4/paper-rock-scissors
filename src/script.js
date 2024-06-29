a51cd074dcaf75f944e57ae8ec80a1f15dcf1814
danielveg4 <dvgilles@gmail.com>
2024-06-29 19:24:00 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -262,8 +262,64 @@ const upperWord = word => {
         }
 }
 console.log(newWord.join(''))
+}
+
+upperWord('mayusculas')
+
+/* crea una funcion que reciba un array con 5 numeros del 1 al 10.
+recórrelo y en el bucle genera un numero aleatorio entre el 0 y el numero del array de la vuelta
+la funcion debe decir si el numero generado está en el array y en qué posicion */
+
+
+const numbersToTenRandom = numbers => {
+    if (!Array.isArray(numbers)) {
+        console.log('introduce un array');
+        return;
+    }
+    if (numbers.length !== 5) {
+        console.log('introduce 5 numeros en el array');
+        return;
+    }
+    
+    for (const number of numbers) {
+        const newRandomNumber = Math.floor(Math.random() * number);
+        if (numbers.includes(newRandomNumber)){
+            console.log('incluye el número ' + number + ' que además está en la posición ' + numbers.indexOf(number))
+        } else {console.log('El número ' + newRandomNumber + ' no está en el array')}
+    }
+
 
 }
 
 
-upperWord('mayusculas')
+numbersToTenRandom([1, 3, 5, 7, 9])
+
+/* crea una función que reciba una frase e imprima el numero de veces que
+se repite cada vocal */
+
+const vowelsPhrase = phrase => {
+    let counterA = 0;
+    let counterE = 0;
+    let counterI = 0;
+    let counterO = 0;
+    let counterU = 0;
+    vowels = ['a', 'e', 'i', 'o', 'u'];
+    for (const letter of phrase) {
+        if (letter === 'a') {
+            counterA++;
+        } else if (letter === 'e') {
+            counterE++;
+        } else if (letter === 'i') {
+            counterI++;
+        } else if (letter === 'o') {
+            counterO++;
+        } else if (letter === 'u') {
+            counterU++;
+        }
+    }
+    console.log(`La vocal a se repite ${counterA} veces, la e ${counterE} veces, la i ${counterI} veces, la u ${counterU} veces y la o ${counterO} veces`)
+
+}
+
+
+vowelsPhrase('esto es una frase espectacular')
