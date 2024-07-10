5748b45ceb1f244c34f0c4699a29e66364f9227b
danielveg4 <dvgilles@gmail.com>
2024-07-10 20:44:18 +0200

    password compleja

--- src/script.js
+++ src/script.js
@@ -1011,30 +1011,66 @@ finalFormElement.addEventListener('input', validateForm)
         Identificar los eventos: cambio del input y click del botón
 */
 
-const passwordCampElement = document.getElementById('password-camp')
-const lengthTextElement = document.getElementById('length-text')
-const inputRangeElement = document.getElementById('input-range')
-const buttonPasswordElement = document.getElementById('button-password')
+const passwordCampElement = document.getElementById('password-camp');
+const lengthTextElement = document.getElementById('length-text');
+const inputRangeElement = document.getElementById('input-range');
+const buttonPasswordElement = document.getElementById('button-password');
+const uppercaseInputElement = document.getElementById('uppercase-input');
+const lowercaseInputElement = document.getElementById('lowercase-input');
+const numbersInputElement = document.getElementById('numbers-input');
+const symbolsInputElement = document.getElementById('symbols-input');
 
-const allowedCharacters = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890+-.,!"·$%&/()=?{}';
+let allowedCharacters = ''
+const uppercaseCharacters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+const lowercaseCharacters = 'abcdefghijklmnñopqrstuvwxyz';
+const numbersCharacters = '1234567890';
+const symbolsCharacters = '+-.,!"·$%&/()=?{}';
+let numeroCaracteres = 16;
 
-console.log(inputRangeElement.value)
+const setPasswordLength = event => {
+    lengthTextElement.textContent = `Length: ${inputRangeElement.value}`;
+}
+
+const checkPasswordOptions = () => {
+    allowedCharacters = '';
+    if (uppercaseInputElement.checked) {
+        allowedCharacters += uppercaseCharacters;
+    }
+    if (lowercaseInputElement.checked) {
+        allowedCharacters += lowercaseCharacters
+    }
+    if (numbersInputElement.checked) {
+        allowedCharacters += numbersCharacters
+    }
+    if (symbolsInputElement.checked) {
+        allowedCharacters += symbolsCharacters
+    }
+
+    return allowedCharacters;
+   
+}
 
 const getPassword = () => {
     let generatedPassword = '';
+    const allowedCharacters = checkPasswordOptions();
     const numeroCaracteres = inputRangeElement.value;
     for (let i = 0; i < numeroCaracteres; i++) {
         const randomNumberPassword = Math.floor(Math.random() * allowedCharacters.length);
         generatedPassword += allowedCharacters[randomNumberPassword];
     }
-    console.log(generatedPassword);
     return generatedPassword;
 }
 
 const printPassword = () => {
     const generatedPassword = getPassword();
     passwordCampElement.textContent = generatedPassword;
-    lengthTextElement.textContent = `Length: ${inputRangeElement.value}`
 }
 
-buttonPasswordElement.addEventListener('click', printPassword)
+
+
+buttonPasswordElement.addEventListener('click', printPassword);
+inputRangeElement.addEventListener('input', setPasswordLength);
+uppercaseInputElement.addEventListener('change', checkPasswordOptions)
+lowercaseInputElement.addEventListener('change', checkPasswordOptions)
+numbersInputElement.addEventListener('change', checkPasswordOptions)
+symbolsInputElement.addEventListener('change', checkPasswordOptions)
